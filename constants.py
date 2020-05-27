61da045caa021a2440923eb7a12106aac838916b
Lucas Rebscher <lucas.rebscher@sprylab.com>
2020-05-27 15:58:40 +0200

    Wrap code into package

--- constants.py
+++
@@ -1,9 +0,0 @@
-import models
-import plots_out
-import os
-
-MODELS_PATH = os.path.dirname(models.__file__)
-print(MODELS_PATH)
-
-PLOTS_PATH = os.path.dirname(plots_out.__file__)
-print(PLOTS_PATH)
---
+++ synchronization/constants.py
@@ -0,0 +1,9 @@
+import models
+import plots_out
+import os
+
+MODELS_PATH = os.path.dirname(models.__file__)
+print(MODELS_PATH)
+
+PLOTS_PATH = os.path.dirname(plots_out.__file__)
+print(PLOTS_PATH)
