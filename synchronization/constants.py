8b93419d367481b43702a39909d85dfd5fcf33ee
Lucas Rebscher <lrebscher@elnath.ni.tu-berlin.de>
2020-06-24 18:32:52 +0200

    Update exploration notebook

--- synchronization/constants.py
+++ synchronization/constants.py
@@ -6,7 +6,12 @@ MODELS_PATH = os.path.dirname(models.__file__)
 PLOTS_PATH = os.path.dirname(plots_out.__file__)
 
 # Remote path to data files on cursa server.
-REMOTE_PATH = "/mnt/raid/data/lrebscher/data"
+
+# cursa
+# REMOTE_PATH = "/mnt/raid/data/lrebscher/data"
+
+# elnath
+REMOTE_PATH = "/mnt/scratch/lrebscher"
 
 
 def get_base_path(remote: bool = False) -> str:
