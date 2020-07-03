b02c43d8cd9e283bc7b1a859a62dfd99725fe518
Lucas Rebscher <luc.rebscher@gmail.com>
2020-07-03 13:31:35 +0200

    Update exploration

--- synchronization/explorations.py
+++ synchronization/explorations.py
@@ -78,32 +78,25 @@ def mean_noise_exploration(remote: bool = True):
 
 def poisson_strength_and_ratio(remote: bool = True):
     default_params = {
-        "runtime": 5000,
+        "runtime": 4000,
         "J_itoi": 5.0,
         "J_etoe": 0.6,
-        "J_etoi": 1.2,
+        "J_etoi": 3.2,
         "J_itoe": 7.0,
-        "N_e": 1000,
-        "N_i": 250,
         "ou_enabled": [False, False],
         "poisson_enabled": [True, True],
         "poisson_variance": 1.0,
         "poisson_p": 0.83,
-        "poisson_mean_input": 200,
+        "poisson_mean_input": 600,
         "poisson_size": 800,
-        "J_ppee": 2.0,
-        "J_ppei": 2.0,
-        # if set to default of 0.1 this leads to strange split in frequency band
+        "J_ppee": 0.6,
+        "J_ppei": 0.6,
         "const_delay": 0.2,
         "N_pop": 2,
-        "p_etoe": 0.1,
-        "p_etoi": 0.4,
-        "p_itoe": 0.1,
-        "p_itoi": 0.4,
     }
 
     params = {
-        "poisson_variance": np.arange(0.1, 2, 0.1),
+        "poisson_variance": np.arange(1.0, 20, 0.25),
         "poisson_p": np.arange(0.75, 1, 0.025),
     }
 
@@ -112,11 +105,16 @@ def poisson_strength_and_ratio(remote: bool = True):
         explore_params=params,
         default_params=default_params,
         hdf_filename=constants.get_base_path(remote) + "/uncorrelated_noise.h5",
-        exploration_name="uncorrelated_noise_3",
+        exploration_name="1",
     )
 
     ex.run()
 
 
 # List of all relevant explorations.
-explorations = [synaptic_weight_exploration, mean_noise_exploration, poisson_strength_and_ratio]
+explorations = [
+    synaptic_weight_exploration,
+    mean_noise_exploration,
+    poisson_strength_and_ratio,
+]
+
